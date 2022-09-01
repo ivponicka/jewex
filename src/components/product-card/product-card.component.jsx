4cd32b43600899e017cd04c09410ce7a1342daea
ivponicka <56002673+ivponicka@users.noreply.github.com>
2022-09-01 11:49:58 +0200

    added addToCart freature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,13 @@
 import './product-card.styles.scss'
 import Button from '../button/button.component';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/toggleCart.context';
 
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product;
+    const {addItemToCard} = useContext(CartContext);
+    const addProductToCard = () => addItemToCard(product);
+
     return (
         <div className='product-card-container'>
             <img src={imageUrl} alt={`${name}`} />
@@ -10,7 +15,7 @@ const ProductCard = ({product}) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted' onClick={addProductToCard} >Add to card</Button>
         </div>
     )
 }
